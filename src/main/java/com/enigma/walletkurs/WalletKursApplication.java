d6de57d4b5c748f3a8a5bdc1d885ebf48f36cfa9
Rifqi Abrory <rifqi.upm@gmail.com>
2019-05-27 11:12:02 +0700

    initial commit

---
+++ src/main/java/com/enigma/walletkurs/WalletKursApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.walletkurs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletKursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletKursApplication.class, args);
+	}
+
+}
