c96a84b37a3bbec017e41d6af8103a83dd76b140
Rifqi Abrory <rifqi.upm@gmail.com>
2019-05-29 03:28:42 +0700

    update account modul

--- src/main/java/com/enigma/walletkurs/controller/AccountController.java
+++ src/main/java/com/enigma/walletkurs/controller/AccountController.java
@@ -3,12 +3,7 @@ package com.enigma.walletkurs.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.enigma.walletkurs.dao.AccountDao;
 import com.enigma.walletkurs.exception.EntityNotFoundException;
@@ -20,57 +15,68 @@ import com.enigma.walletkurs.models.CustomerEntity;
 @RestController
 public class AccountController {
 
-	public static final String URI_REQUEST_ACCOUNT = "account";
-	public static final String URI_REQUEST_ACCOUNT_BY_ACCOUNT_NUMBER = "/account/{accountNumber}";
-	public static final String URL_REQUEST_ACCOUNTS_BY_CIF = "/accountcif/{customerNumber}";
+    public static final String URI_REQUEST_ACCOUNT = "account";
+    public static final String URI_REQUEST_ACCOUNT_BY_ACCOUNT_NUMBER = "account/{accountNumber}";
+    public static final String URI_REQUEST_ACCOUNTS_BY_CUSTOMER_NUMBER = "accounts/{customerNumber}";
 
-	@Autowired
-	private AccountDao accountDao;
+    @Autowired
+    private AccountDao accountDao;
 
-	@PostMapping(value = URI_REQUEST_ACCOUNT)
-	public CommonResponse<AccountEntity> add(@RequestBody AccountEntity account) {
-		CommonResponse<AccountEntity> acc = new CommonResponse<>();
-		AccountEntity tempAcc = accountDao.addAccount(account);
-		acc.setData(tempAcc);
-		return acc;
-	}
+    @PostMapping(value = URI_REQUEST_ACCOUNT)
+    public CommonResponse<AccountEntity> add(@RequestBody AccountEntity account) {
+        CommonResponse<AccountEntity> acc = new CommonResponse<>();
+        AccountEntity tempAcc = accountDao.create(account);
+        acc.setData(tempAcc);
+        return acc;
+    }
 
-	@PutMapping(value = URI_REQUEST_ACCOUNT_BY_ACCOUNT_NUMBER)
-	public CommonResponse<AccountEntity> update(@RequestBody AccountEntity account) throws NotFoundException {
-		AccountEntity acc = accountDao.getByAccountNumber(account.getAccountNumber());
-		CommonResponse<AccountEntity> response = new CommonResponse<>();
-		if (acc == null) {
-			throw new NotFoundException(44, "Customer data doesn't exist!");
-		} else {
-			accountDao.updateAccount(account);
-			response.setData(account);
-		}
-		return response;
-	}
+    @PutMapping(value = URI_REQUEST_ACCOUNT_BY_ACCOUNT_NUMBER)
+    public CommonResponse<AccountEntity> update(@RequestBody AccountEntity account) throws NotFoundException {
+        AccountEntity acc = accountDao.getByAccountNumber(account.getAccountNumber());
+        CommonResponse<AccountEntity> response = new CommonResponse<>();
+        if (acc == null) {
+            throw new NotFoundException(44, "Customer data doesn't exist!");
+        } else {
+            accountDao.update(account);
+            response.setData(account);
+        }
+        return response;
+    }
 
-	@GetMapping(value = URI_REQUEST_ACCOUNT_BY_ACCOUNT_NUMBER)
-	public CommonResponse<AccountEntity> getByAccount(@PathVariable(name = "accountNumber") String accountNumber)
-			throws EntityNotFoundException {
-		AccountEntity account = accountDao.getByAccountNumber(accountNumber);
-		CommonResponse<AccountEntity> response = new CommonResponse<>();
-		if (account == null) {
-			throw new EntityNotFoundException("44", String.format("Student ID %d not found", accountNumber));
-		} else {
-			response.setData(account);
-		}
-		return response;
-	}
+    @GetMapping(value = URI_REQUEST_ACCOUNT_BY_ACCOUNT_NUMBER)
+    public CommonResponse<AccountEntity> getByAccount(@PathVariable(name = "accountNumber") String accountNumber) throws NotFoundException {
+        AccountEntity account = accountDao.getByAccountNumber(accountNumber);
+        CommonResponse<AccountEntity> response = new CommonResponse<>();
+        if (account == null) {
+            throw new NotFoundException(44, String.format("Account ID %d not found", accountNumber));
+        } else {
+            response.setData(account);
+        }
+        return response;
+    }
 
-	@GetMapping(value = URL_REQUEST_ACCOUNTS_BY_CIF)
-	public CommonResponse<List<AccountEntity>> getAccountsByCif(
-			@PathVariable(name = "customerNumber") CustomerEntity customerNumber) throws EntityNotFoundException {
-		List<AccountEntity> listAccount = accountDao.getAccountsByCIF(customerNumber);
-		CommonResponse<List<AccountEntity>> resp = new CommonResponse<>();
-		if (listAccount == null) {
-			throw new EntityNotFoundException("44", String.format("Student ID %d not found", customerNumber));
-		} else {
-			resp.setData(listAccount);
-		}
-		return resp;
-	}
+    @GetMapping(value = URI_REQUEST_ACCOUNTS_BY_CUSTOMER_NUMBER)
+    public CommonResponse<List<AccountEntity>> getAccountsByCif(
+            @PathVariable(name = "customerNumber") CustomerEntity customerNumber) throws EntityNotFoundException {
+        List<AccountEntity> listAccount = accountDao.getAccountsByCustomerNumber(customerNumber);
+        CommonResponse<List<AccountEntity>> resp = new CommonResponse<>();
+        if (listAccount == null) {
+            throw new EntityNotFoundException("44", String.format("Account ID %d not found", customerNumber));
+        } else {
+            resp.setData(listAccount);
+        }
+        return resp;
+    }
+
+    @DeleteMapping(value = URI_REQUEST_ACCOUNT_BY_ACCOUNT_NUMBER)
+    public CommonResponse<AccountEntity> deleteAccount(@PathVariable(name = "accountNumber") String accountNumber) throws NotFoundException {
+        AccountEntity check = accountDao.getByAccountNumber(accountNumber);
+        CommonResponse<AccountEntity> resp = new CommonResponse<>();
+        if (check == null) {
+            throw new NotFoundException(44, String.format("Account ID %d not found", accountNumber));
+        } else {
+            resp.setData(accountDao.delete(check));
+        }
+        return resp;
+    }
 }
